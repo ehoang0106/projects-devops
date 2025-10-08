484cdf76071e10484f9eeeae638fc8a46a8f6af4
Khoa Hoang <khoahoang1698@gmail.com>
2025-10-08 08:46:16 -0700

    first commit

---
+++ project-01/terraform/network/provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+
+  backend "s3" {
+    bucket = "terraform-state-khoa-hoang"
+    key = "terraform_tfstate_devops_project"
+    region = "us-west-1"
+  }
+}
+
+provider "aws" {
+  region = "us-west-1"
+}
